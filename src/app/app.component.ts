541487f17ef8e2a7ee3270839e16d00508831895
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-30 18:51:43 -0300

    Aula 14 - ngDoCheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, AfterContentInit, AfterViewInit, AfterViewChecked, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
   <app-title title="Olá mundo!"></app-title>
+  {{ valor }}
+  <button (click)="adicionar()">Adicionar</button>&nbsp;
+  <button (click)="subtrair()">Subtrair</button>
   <router-outlet></router-outlet>`
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterViewInit, AfterViewChecked, AfterContentChecked {
+
+  public valor: number = 0;
 
   constructor() { }
+
+  public adicionar(): number {
+    return this.valor += 1;
+  }
+
+
+  subtrair(): number {
+    return this.valor -= 1;
+  }
+
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
 
   ngOnInit(): void {
     setTimeout(() => {
