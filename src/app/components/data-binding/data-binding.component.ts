f593b88aa6adfb9f9c3679b4a2ccf36585ce9fe3
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-30 22:04:58 -0300

    Aula 19 - Event Binding

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -15,7 +15,26 @@ export class DataBindingComponent implements OnInit {
   public checkedDisabled: boolean = true;
   public imgSrc: string = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
 
+  public position: { x: number, y: number } = { x: 0, y: 0 };
+
   ngOnInit(): void {
+  }
+
+
+  alertaInfo(): void {
+    alert("Você clicou no botão!");
+  }
+
+
+  eventInfo(event: MouseEvent): void {
+    console.log(event);
+  }
+
+
+  mouseMoveTest(event: MouseEvent): void {
+    console.log(event);
+    this.position.x = event.offsetX;
+    this.position.y = event.offsetY;
   }
 
 }
